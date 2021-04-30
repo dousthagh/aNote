89e079d6749ad1ee9d0ed509115dea29a079a360
dousthagh <mohammad.dousthagh@gmail.com>
2021-04-30 17:08:46 +0430

    user profile ui

--- app/src/main/java/co/nikavtech/anote/database/entities/UserEntity.kt
+++ app/src/main/java/co/nikavtech/anote/database/entities/UserEntity.kt
@@ -1,5 +1,6 @@
 package co.nikavtech.anote.database.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,7 +9,17 @@ data class UserEntity(
     @PrimaryKey(autoGenerate = true)
     var id: Long? = null,
     var email: String? = null,
-    var password: String? = null
+    var password: String? = null,
+
+    @ColumnInfo(name = "first_name")
+    var firstName: String? = null,
+
+    @ColumnInfo(name = "last_name")
+    var lastName: String? = null,
+
+    @ColumnInfo(name = "mobile")
+    var mobile: String? = null,
+
 ){
     fun isCorrect():Boolean{
         return email != null && password != null
